20ace9e8a9a0542d86c27282d0953e12b53d92f4
Kenzo_Johnson <148596766+KenzoJ@users.noreply.github.com>
2024-06-16 13:44:37 -0400

    add lists to home page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,13 @@
 class MainController < ApplicationController
   def index
+    render('index')
+  end
+
+  def about
+    render('about')
+  end
+
+  def hello
+    redirect_to(action: 'index')
   end
 end
